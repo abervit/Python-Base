59873c10d102780dd1e609b9b53c6c90a2acb412
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-14 22:48:17 +0100

    Rename

---
+++ less2_strings.py
@@ -0,0 +1,36 @@
+# Working with strings
+"""Creating a string"""
+print("Giraffe Academy")
+print("Giraffe \nAcademy")
+print("Giraffe \"Academy") # allowing us to have a quotation mark before academy
+
+"""Concatenation"""
+phrase = "Giraffe Academy"
+new_pharase = "Welcome to " + phrase
+print(new_pharase) # --> Welcome to Giraffe Academy
+
+"""Functions"""
+phrase = "Giraffe Academy"
+print(phrase.isalnum()) # --> False
+
+print(phrase.lower()) # --> giraffe academy
+print(phrase.upper()) # --> GIRAFFE ACADEMY
+print(phrase.capitalize()) # --> Giraffe academy
+print(phrase.swapcase()) # --> gIRAFFE aCADEMY
+print(phrase.upper().isupper()) # --> True
+
+print(len(phrase)) # --> 15
+print(phrase.replace("Giraffe", "Crocodile")) # --> Crocodile Academy
+print(phrase[2:5]) # --> raf
+
+"""Index function tells where is the first occurrence of specified character. 
+We also can specify the position space - like between 0th and 10th index of the string"""
+print(phrase.index("a", 0, 15)) # --> 3
+print(phrase.index("a")) # --> 3
+
+"""Some other functions"""
+phrase = "Giraffe Academy"
+print(phrase.count("a")) # --> 2
+print(phrase.count("a", 0, 3)) # --> 0
+
+
