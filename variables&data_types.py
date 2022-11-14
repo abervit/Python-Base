66c51f766195fbb6ede2c70aafce06a55632c46f
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-14 21:28:40 +0100

    Varaibles and Data Types

---
+++ variables&data_types.py
@@ -0,0 +1,14 @@
+# Variables and data types
+
+print("There once was a man named George, ")
+print("He was 70 years old. ")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
+
+name = "Tom"
+age = "25"
+print("\n")
+print(f"There once was a man named {name}, ")
+print(f"He was {age} years old. ")
+print(f"He really liked the name {name}, ")
+print(f"but didn't like being {age}.")
