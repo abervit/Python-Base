71b7d63d59843f8e716bca7ff204597ee1f4f957
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 10:39:07 +0100

    Basic calculator

---
+++ less5_basic_calculator.py
@@ -0,0 +1,8 @@
+# Building a basic calculator
+
+"""By default, python is taking a string as an answer, so we have to convert the number into int or float"""
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = num1 + num2
+print(result)
