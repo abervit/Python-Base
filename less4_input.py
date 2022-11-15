7aed66e52e81233c16f45f0ea8f6f6c68f718805
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 10:32:06 +0100

    Short input from a user

---
+++ less4_input.py
@@ -0,0 +1,6 @@
+# Getting input from users
+
+name = input("Hey, tell me your name: ")
+print(f"Hello, your name is {name}")
+print("Hello, your name is", name)
+print("Hello, your name is " + name)
