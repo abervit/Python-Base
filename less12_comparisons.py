e71f58396e09cdb807ae36d2f479f1cc1dafe943
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 16:14:10 +0100

    If statement and comparisons

---
+++ less12_comparisons.py
@@ -0,0 +1,14 @@
+# If statement and comparisons
+
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3:
+        return num2
+    else:
+        return num3
+
+
+print(max_num(2, 4, 6))
+
+"""We can compare different data types: strings, integers, buleans etc..."""
