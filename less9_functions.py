a6073c99c74cebd61629dbf64ed4edbd50811bac
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 13:07:01 +0100

    Basic of functions

---
+++ less9_functions.py
@@ -0,0 +1,15 @@
+# Functions - a collection of code which performs a specific task
+# Functions help us to organize our code better. Break up it in a little chunks that are doing different things
+
+def say_hi():
+    name = input("Enter your name: ")
+    print(f"Hi, my dear {name}")
+
+say_hi()
+
+"""Passing perimeters to a function. Peace of information that we pass"""
+def greet(name, age, city):
+    print(f"Hello dear {name}. You are {age} years old, and live in {city}.")
+
+greet("Tom", 24, "New York")
+greet("Olef", 43, "Boston")
