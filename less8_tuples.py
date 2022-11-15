99650623c4e457765df0d1da652eeafd2010fb3e
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 12:50:31 +0100

    Tuples

--- less8_tuples.py
+++ less8_tuples.py
@@ -5,4 +5,6 @@ coordinates = (4, 5)
 print(coordinates)  # --> (4, 5)
 print(coordinates[0])  # --> 4
 
-
+"""The difference between tuples and lists - immutability. Usually use for data that doesn't change"""
+coordinates[0] = 23
+print(coordinates)  # --> 'tuple' object does not support item assignment
