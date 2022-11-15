409232af82a58a4f7a275ac1f23127c1c1b73945
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 16:24:29 +0100

    Building a better calculator

--- less5_basic_calculator.py
+++ less5_basic_calculator.py
@@ -6,3 +6,20 @@ num2 = float(input("Enter the second number: "))
 
 result = num1 + num2
 print(result)
+
+# Building a better calculator
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter your operator: ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "*":
+    print(num1 * num2)
+elif operator == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
