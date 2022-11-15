48e578d5f3ca138d1a35afb6ca8e84f29fac867d
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 10:46:48 +0100

    Mad Lib Games

---
+++ less5_mad_libs_game.py
@@ -0,0 +1,9 @@
+# Mad Lib Games
+colour = input("Enter your favourite colour: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter celebrity: ")
+
+print(f"Roses are {colour}")
+print(f"{plural_noun} are blue")
+print(f"I love {celebrity}!!")
+
