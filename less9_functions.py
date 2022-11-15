649ed42767a49bea66feca0f15ee9abd4e33d214
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 13:09:16 +0100

    Added some answers for functions

--- less9_functions.py
+++ less9_functions.py
@@ -5,11 +5,15 @@ def say_hi():
     name = input("Enter your name: ")
     print(f"Hi, my dear {name}")
 
-say_hi()
+
+say_hi()  # --> Enter your name: Kev ==> Hi, my dear Kev
 
 """Passing perimeters to a function. Peace of information that we pass"""
+
+
 def greet(name, age, city):
     print(f"Hello dear {name}. You are {age} years old, and live in {city}.")
 
-greet("Tom", 24, "New York")
-greet("Olef", 43, "Boston")
+
+greet("Tom", 24, "New York")  # --> Hello dear Tom. You are 24 years old, and live in New York.
+greet("Olef", 43, "Boston")  # --> Hello dear Olef. You are 43 years old, and live in Boston.
