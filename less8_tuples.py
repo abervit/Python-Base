1486bc9fcc7159d76086360aab4a1546ff87c361
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-15 12:47:03 +0100

    Rename

---
+++ less8_tuples.py
@@ -0,0 +1,8 @@
+# Tuples - a container (data structure) where we can store different values
+
+"""Tuple creation. Tuple is immutable, can't be changed or modified"""
+coordinates = (4, 5)
+print(coordinates)  # --> (4, 5)
+print(coordinates[0])  # --> 4
+
+
