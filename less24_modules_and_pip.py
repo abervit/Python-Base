5ba2df7b075379da3609a8f40085ba584f71b9d8
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-16 14:34:47 +0100

    Downloading modules, their location and pip

---
+++ less24_modules_and_pip.py
@@ -0,0 +1,18 @@
+# Modules and Pip
+# Module a python file that you can import into our current python file and access its functions
+
+"""Python module index <--> look for information about modules"""
+import useful_tools
+
+print(useful_tools.roll_dice(7))  # --> gives us a random num
+
+"""How to find all modules that are stored - some are built-in other are external"""
+"""Go to External Libraries -> Lib (this folder stores all external modules)"""
+
+"""Pip comes pre-installed with python 3++ version. We use pip to install python modules"""
+"""It refers as a package manager - allows us to install, manage update or unistall python modules"""
+
+"""When we want to find the module that we installed - below look"""
+"""Go to External Libraries - Lib - site packages"""
+
+import docx
