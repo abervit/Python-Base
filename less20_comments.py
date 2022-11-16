6587a9002a1ee0fca7f6499418c81b45f10cb416
abervit <vitaliy.kachkovskiy@gmail.com>
2022-11-16 10:23:41 +0100

    Quick comments

---
+++ less20_comments.py
@@ -0,0 +1,4 @@
+# Comments - a line which is not executed by python, used for developers to make some comments
+
+# The coolest guy in the world -> won`t be seen
+print("Not the coolest guy in the world")
